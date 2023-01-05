0c55f8f4699b4b5ce30e1ccde65424304af50d7b
max.kulbako <mkulbako@gmail.com>
2023-01-05 19:49:32 +0100

    fix @lias

--- craco.config.js
+++ craco.config.js
@@ -1,11 +1,15 @@
 const path = require(`path`);
 
 module.exports = {
+    mode: 'development',
+        devServer: {
+        port: 8001,
+        },
     webpack: {
         alias: {
             '@': path.resolve(__dirname, 'src/'),
-            '@store': path.resolve(__dirname, 'src/store'),
-            '@views': path.resolve(__dirname, 'src/views'),
+            '@store': path.resolve(__dirname, 'src/store/'),
+            '@views': path.resolve(__dirname, 'src/views/*'),
         }
     },
 };
--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import {Provider as ReduxProvider} from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 import {store} from '@store';
-import {routes} from './store/router';
-
+import {routes} from '@store/router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
